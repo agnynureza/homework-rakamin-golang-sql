0fea2a790d5078d3ae5585d2050a5bae128f5ecf
agnynureza <agny.nureza@amartha.com>
2021-08-29 20:36:12 +0700

    add unit testing repository

--- repository/movies_test.go
+++ repository/movies_test.go
@@ -11,19 +11,24 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitMockDB(t *testing.T) (*MoviesRepository, sqlmock.Sqlmock, *sql.DB) {
-	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+var (
+	repo *MoviesRepository
+	mock sqlmock.Sqlmock
+	db   *sql.DB
+	err  error
+)
+
+func InitMockDB(t *testing.T) {
+	db, mock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
 		t.Fatalf("Error initialize DATA-DOG : %v", err)
 	}
 	gormDB, _ := gorm.Open(mysql.New(mysql.Config{Conn: db}), &gorm.Config{})
-	repo := NewMoviesRepository(gormDB)
-
-	return repo, mock, db
+	repo = NewMoviesRepository(gormDB)
 }
 
 func TestMoviesRepository_CreateMovie(t *testing.T) {
-	repo, mock, db := InitMockDB(t)
+	InitMockDB(t)
 	defer db.Close()
 
 	t.Run("should return success", func(t *testing.T) {
@@ -37,9 +42,11 @@ func TestMoviesRepository_CreateMovie(t *testing.T) {
 		outputMovieID := 1
 		queryExpected := "INSERT INTO `movies` (`title`,`slug`,`description`,`duration`,`image`) VALUES (?,?,?,?,?)"
 
+		mock.ExpectBegin()
 		mock.ExpectExec(queryExpected).
 			WillReturnResult(sqlmock.NewResult(1, 1)).
 			WillReturnError(nil)
+		mock.ExpectCommit()
 
 		actualMovieID, err := repo.CreateMovie(&inputMovie)
 		assert.NoError(t, err)
@@ -48,7 +55,7 @@ func TestMoviesRepository_CreateMovie(t *testing.T) {
 }
 
 func TestMoviesRepository_GetOneMovie(t *testing.T) {
-	repo, mock, db := InitMockDB(t)
+	InitMockDB(t)
 	defer db.Close()
 
 	t.Run("should return success", func(t *testing.T) {
@@ -71,5 +78,48 @@ func TestMoviesRepository_GetOneMovie(t *testing.T) {
 		actual, err := repo.GetOneMovie(inputSlug)
 		assert.Nil(t, err)
 		assert.Equal(t, actual, ouputMovieDetail)
+	})
+}
+
+func TestMoviesRepository_UpdateMovie(t *testing.T) {
+	InitMockDB(t)
+	defer db.Close()
+
+	t.Run("should return success", func(t *testing.T) {
+		inputMovie := models.Movies{
+			Title:       "Titanic version 2",
+			Slug:        "boboboy",
+			Description: "lorem ipsum",
+			Duration:    60,
+			Image:       "image titanic URL",
+		}
+		inputSlug := "titanic"
+		queryExpected := "UPDATE movies SET title = ?, description = ?, duration = ?, image = ?, slug = ? WHERE slug = ?"
+
+		mock.ExpectExec(queryExpected).
+			WillReturnResult(sqlmock.NewResult(1, 1)).
+			WillReturnError(nil)
+
+		err := repo.UpdateMovie(&inputMovie, inputSlug)
+		assert.NoError(t, err)
+	})
+}
+
+func TestMoviesRepository_DeleteMovie(t *testing.T) {
+	InitMockDB(t)
+	defer db.Close()
+
+	t.Run("should return success", func(t *testing.T) {
+		inputSlug := "titanic"
+		queryExpected := "DELETE FROM `movies` WHERE slug = ?"
+
+		mock.ExpectBegin()
+		mock.ExpectExec(queryExpected).
+			WillReturnResult(sqlmock.NewResult(1, 1)).
+			WillReturnError(nil)
+		mock.ExpectCommit()
+
+		err := repo.DeleteMovie(inputSlug)
+		assert.NoError(t, err)
 	})
 }
